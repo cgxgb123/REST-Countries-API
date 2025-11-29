15b44aa89ed84521c53442e536028422a87164c0
Christian Blunt <christian.blunt12@gmail.com>
2025-11-29 18:23:22 -0500

    added dark/light mode theme functionality and persistence on country.html page, ensured loadCountry() is triggered after DOM loads

--- utils/country.js
+++ utils/country.js
@@ -1,3 +1,34 @@
 const toggle = document.getElementById("dark-mode");
-const Details = document.getElementById("country-details");
+const Details = document.getElementById("country-detail");
 const toggleText = document.querySelector("span");
+const toggleIcon = document.querySelector("i");
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const saved = localStorage.getItem("theme");
+
+  if (saved === "dark") {
+    document.body.classList.add("dark-theme");
+    toggleIcon.className = "fa-solid fa-sun";
+    toggleText.textContent = "Light Mode";
+  }
+
+  loadCountry();
+});
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+
+  const isDark = document.body.classList.contains("dark-theme");
+
+  if (isDark) {
+    toggleIcon.className = "fa-solid fa-sun";
+    toggleText.textContent = "Light Mode";
+    localStorage.setItem("theme", "dark");
+  } else {
+    toggleIcon.className = "fa-regular fa-moon";
+    toggleText.textContent = "Dark Mode";
+    localStorage.setItem("theme", "light");
+  }
+});
